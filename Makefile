915d216e8f3df718911e47f9cc005fe91ceaba09
wav <scaryjoyastik@gmail.com>
2026-04-28 15:53:15 +0300

    fixes

--- Makefile
+++ Makefile
@@ -57,3 +57,12 @@ todoapp-run:
 	export POSTGRES_HOST=localhost && \
 	go mod tidy && \
 	go run cmd/todoapp/main.go
+
+logs-cleanup:
+	@read -p "Очистить все log файлы окружения? Опасность утери логов. [y/N]: " ans; \
+	if [ "$$ans" = "y" ]; then \
+		rm -rf ${PROJECT_ROOT}/out/logs && \
+		echo "Файлы логов очищены"; \
+	else \
+		echo "Очистка логов отменена"; \
+	fi;
--- cmd/todoapp/main.go
+++ cmd/todoapp/main.go
@@ -8,6 +8,7 @@ import (
 	"syscall"
 	"time"
 
+	core_config "github.com/wavw1/golang-todoapp/internal/core/config"
 	core_logger "github.com/wavw1/golang-todoapp/internal/core/logger"
 	core_postgres_pool "github.com/wavw1/golang-todoapp/internal/core/repository/postgres/pool"
 	core_http_middleware "github.com/wavw1/golang-todoapp/internal/core/transport/http/middleware"
@@ -21,12 +22,9 @@ import (
 	"go.uber.org/zap"
 )
 
-var (
-	timeZone = time.UTC
-)
-
 func main() {
-	time.Local = timeZone
+	cfg := core_config.NewConfigMust()
+	time.Local = cfg.TimeZone
 
 	ctx, cancel := signal.NotifyContext(
 		context.Background(),
@@ -41,7 +39,7 @@ func main() {
 	}
 	defer logger.Close()
 
-	logger.Debug("application time zone", zap.Any("zone", timeZone))
+	logger.Debug("application time zone", zap.Any("zone", time.Local))
 
 	logger.Debug("initializing postgres connection pool")
 	pool, err := core_postgres_pool.NewConnectionPool(
